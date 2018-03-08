54d19c145f9f859a2e82ac4f1760d664d08744ff
leo <leo.ertuna@gmail.com>
2018-03-08 22:00:55 +0200

    Reworked language enum, added more languages, added vector support

--- src/main/java/com/tekleo/language_classifier/dictionaries/Language.java
+++ src/main/java/com/tekleo/language_classifier/dictionaries/Language.java
@@ -4,22 +4,72 @@ import java.util.LinkedList;
 import java.util.List;
 
 public enum Language {
-    CZECH(1), ENGLISH(2), GERMAN(3), RUSSIAN(4), TURKISH(5);
+    // Values
+    //------------------------------------------------------------------------------------------------------------------
+    ARMENIAN(1),
+    BASQUE(2),
+    BULGARIAN(3),
+    CATALAN(4),
+    CROATIAN(5),
+    CZECH(6),
+    DANISH(7),
+    DUTCH(8),
+    ENGLISH(9),
+    ESTONIAN(10),
+    FRENCH(11),
+    GALEGO(12),
+    GERMAN(13),
+    GREEK(14),
+    HUNGARIAN(15),
+    ICELANDIC(16),
+    INDONESIAN(17),
+    ITALIAN(18),
+    KOREAN(19),
+    LATIN(20),
+    LATVIAN(21),
+    LITHUANIAN(22),
+    LUXEMBOURGISH(23),
+    MALAYS(24),
+    MONGOLIAN(25),
+    NORWEGIAN(26),
+    POLISH(27),
+    PORTUGUESE(28),
+    ROMANIAN(29),
+    RUSSIAN(30),
+    SERBIAN(31),
+    SLOVENIAN(32),
+    SPANISH(33),
+    SWEDISH(34),
+    TURKISH(35),
+    UKRAINIAN(36),
+    VIETNAMESE(37);
+    //------------------------------------------------------------------------------------------------------------------
 
-    private int value;
 
-    Language(int value) {
-        this.value = value;
+
+    // Enum
+    //------------------------------------------------------------------------------------------------------------------
+    private int asInt;
+
+    Language(int asInt) {
+        this.asInt = asInt;
     }
 
-    public int getValue() {
-        return value;
+    public int getAsInt() {
+        return asInt;
+    }
+
+    public int[] getAsVector() {
+        int[] v = new int[37];
+        v[asInt - 1] = 1;
+        return v;
     }
 
     public static List<Integer> getAllPossibleValues() {
         List<Integer> values = new LinkedList<>();
         for (Language language : values())
-            values.add(language.getValue());
+            values.add(language.getAsInt());
         return values;
     }
+    //------------------------------------------------------------------------------------------------------------------
 }
