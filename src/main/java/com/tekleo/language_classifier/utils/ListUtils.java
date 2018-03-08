a57a245243aa588f83ed4204010191e74e325856
leo <leo.ertuna@gmail.com>
2018-03-08 15:07:14 +0200

    Split method for list utils

--- src/main/java/com/tekleo/language_classifier/utils/ListUtils.java
+++ src/main/java/com/tekleo/language_classifier/utils/ListUtils.java
@@ -1,7 +1,10 @@
 package com.tekleo.language_classifier.utils;
 
+import org.nd4j.linalg.primitives.Pair;
+
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
 public class ListUtils {
     public static <E> LinkedList<E> toLinkedList(E[] a) {
@@ -16,5 +19,18 @@ public class ListUtils {
         for (E e : a)
             list.add(e);
         return list;
+    }
+
+    public static <E> ArrayList<E> toArrayList(List<E> a) {
+        ArrayList<E> list = new ArrayList<>(a.size());
+        list.addAll(a);
+        return list;
+    }
+
+    public static <E> Pair<ArrayList<E>, ArrayList<E>> split(List<E> list, double ratio) {
+        int index = (int) (list.size() * ratio);
+        ArrayList<E> slice1 = toArrayList(list.subList(0, index));
+        ArrayList<E> slice2 = toArrayList(list.subList(index, list.size()));
+        return new Pair<>(slice1, slice2);
     }
 }
