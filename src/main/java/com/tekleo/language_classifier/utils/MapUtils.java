29876cb0f48f44be8a76eee641d6fb27cb4c8fc1
leo <leo.ertuna@gmail.com>
2018-03-09 01:15:32 +0200

    Created map utils

---
+++ src/main/java/com/tekleo/language_classifier/utils/MapUtils.java
@@ -0,0 +1,21 @@
+package com.tekleo.language_classifier.utils;
+
+import java.util.*;
+
+public class MapUtils {
+    public static <K, V extends Comparable<? super V>> LinkedHashMap<K, V> sortByValue(Map<K, V> map) {
+        List<Map.Entry<K, V>> list = new LinkedList<>(map.entrySet());
+        Collections.sort( list, new Comparator<Map.Entry<K, V>>() {
+            @Override
+            public int compare(Map.Entry<K, V> o1, Map.Entry<K, V> o2) {
+                return - (o1.getValue()).compareTo(o2.getValue());
+            }
+        });
+
+        LinkedHashMap<K, V> result = new LinkedHashMap<>();
+        for (Map.Entry<K, V> entry : list) {
+            result.put(entry.getKey(), entry.getValue());
+        }
+        return result;
+    }
+}
