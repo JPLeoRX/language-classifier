923f35934d16969a0bcd0265c5f05c1bc0d0875d
leo <leo.ertuna@gmail.com>
2018-03-08 15:50:19 +0200

    Showing how to build data set from txt files in Main class

--- src/main/java/com/tekleo/language_classifier/Main.java
+++ src/main/java/com/tekleo/language_classifier/Main.java
@@ -2,23 +2,21 @@ package com.tekleo.language_classifier;
 
 import com.tekleo.language_classifier.dictionaries.Dictionary;
 import com.tekleo.language_classifier.dictionaries.Language;
-import com.tekleo.language_classifier.dictionaries.Word;
-import com.tekleo.language_classifier.utils.FileUtils;
-import com.tekleo.language_classifier.utils.StringUtils;
-import org.deeplearning4j.datasets.fetchers.BaseDataFetcher;
-import org.deeplearning4j.datasets.iterator.BaseDatasetIterator;
-import org.deeplearning4j.datasets.iterator.DoublesDataSetIterator;
-
-import java.util.List;
+import com.tekleo.language_classifier.neural_network.WordsDataSet;
+import com.tekleo.language_classifier.neural_network.WordsDataSetBuilder;
 
 public class Main {
     public static void main(String[] args) {
-        // Load dictionary
-        Dictionary d = new Dictionary("english.txt", Language.ENGLISH);
+        // Load dictionaries
+        Dictionary dictionaryEnglish = new Dictionary("english.txt", Language.ENGLISH);
+        Dictionary dictionaryGerman = new Dictionary("german.txt", Language.GERMAN);
 
-        // Convert it to data set for machine learning
-        DoublesDataSetIterator i = new DoublesDataSetIterator(d, 10);
+        // Create data set
+        WordsDataSet dataSet = new WordsDataSetBuilder().setRatio(0.8).setBatchSize(10).addDictionary(dictionaryEnglish).addDictionary(dictionaryGerman).build();
 
-
+        // Check its data
+        System.out.println("Total words in the data set: " + dataSet.getSizeTotal());
+        System.out.println("Training words in the data set: " + dataSet.getSizeTraining());
+        System.out.println("Testing words in the data set: " + dataSet.getSizeTesting());
     }
 }
