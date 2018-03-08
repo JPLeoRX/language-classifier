948f5de3a7bf5cc5d9db278d139ba02e6007b578
leo <leo.ertuna@gmail.com>
2018-03-08 02:39:51 +0200

    Minor improvements for array utils

--- src/main/java/com/tekleo/language_classifier/utils/ArrayUtils.java
+++ src/main/java/com/tekleo/language_classifier/utils/ArrayUtils.java
@@ -1,10 +1,18 @@
 package com.tekleo.language_classifier.utils;
 
 public class ArrayUtils {
+    public static int[] fromOne(int i) {
+        return new int[]{i};
+    }
+
     public static double[] toDoublesArray(int[] intArray) {
         double[] doublesArray = new double[intArray.length];
         for (int index = 0; index < intArray.length; index++)
             doublesArray[index] = (double) intArray[index];
         return doublesArray;
+    }
+
+    public static double[] toDoublesArray(int i) {
+        return toDoublesArray(fromOne(i));
     }
 }
